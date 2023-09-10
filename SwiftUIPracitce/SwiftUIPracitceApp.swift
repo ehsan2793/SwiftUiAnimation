aeb97f830b7a73af4e7fd55c7e0f4939876201ca
ehsan2793 <ehsan2793@gmail.com>
2023-09-10 16:47:33 -0700

    Initial Commit

---
+++ SwiftUIPracitce/SwiftUIPracitceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracitceApp.swift
+//  SwiftUIPracitce
+//
+//  Created by Ehsan Rahimi on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracitceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
