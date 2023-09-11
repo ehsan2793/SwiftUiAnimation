7bc0e3a01509c593709786029e3ba18f7883bd83
ehsan2793 <ehsan2793@gmail.com>
2023-09-10 18:39:48 -0700

    first commit

---
+++ SwiftUIPracitce/AnimatingCircle.swift
@@ -0,0 +1,26 @@
+//
+//  AnimatingCircle.swift
+//  SwiftUIPracitce
+//
+//  Created by Ehsan Rahimi on 9/10/23.
+//
+
+import SwiftUI
+
+struct AnimatingCircle: View {
+    // MARK: - PROPERTIES
+
+    // MARK: - BODY
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: PREVIEW
+
+struct AnimatingCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimatingCircle()
+    }
+}
